90b55a1a20c18598bc2ab011faaf838531b61632
Rocky.C <cloki0610@gmail.com>
2021-10-30 16:36:40 +0000

    The GitHub API

---
+++ assets/js/github-information.js
@@ -0,0 +1,80 @@
+function fetchGitHubInformation(event) {
+    $("gh-user-data").html("");
+    $("gh-repo-data").html("");
+
+    let username = $("#gh-username").val();
+
+    if (!username) {
+        $("gh-user-data").html("<h2>Please enter a GitHub username</h2>");
+        return;
+    }
+
+    $("gh-user-data").html(
+        `<div id="loader">
+            <img src="assets/css/loader.gif" alt="loading..." />
+        </div>`
+    );
+
+    $when(
+        $.getJSON(`https://api.gihub.com/user/${username}`),
+        $.getJSON(`https://api.gihub.com/user/${username}/repos`)
+    )
+    .then(
+        (firstResponse, secondResponse) => {
+            let userData = firstResponse[0];
+            let repoData = secondResponse[0];
+            $("gh-user-data").html(userInformationHTML(userData));
+            $("gh-repo-data").html(repoInformationHTML(repoData));
+        }, (errorResponse) => {
+            if (errorResponse.status === 404) {
+                $("gh-user-data").html(`<h2>No info found for ${username}</h2>`);
+            } else if (errorResponse.status === 403) {
+                let resetTime = new Date(errorResponse.getResponseHeader('X-RateLimit-Reset') * 1000);
+                $("gh-user-data").html(`<h4>Too many requests, please wait until ${resetTime.toLocaleTimeString} and try again.</h4>`);
+            } else {
+                console.log(errorResponse);
+                $("gh-user-data").html(`<h2>Error: ${errorResponse.responseJSON.message}</h2>`);
+            }
+        }
+    )
+}
+
+function userInformationHTML(user) {
+    return `
+        <h2>${user.name}
+            <span class="small-name">
+            (@<a href="${user.html_url}" target="_blank">${user.login}</a>)
+        </span>
+        </h2>
+        <div class="gh-content">
+            <div class="gh-avatar">
+                <a href="${user.html_url}" target="_blank">
+                    <img src="${user.avatar_url}" width="80" height="80" alt="${user.login}" />
+                </a>
+            </div>
+            <p>Followers: ${user.followers} - Following ${user.following} <br> Repos: ${user.public_repos}</p>
+        </div>`;
+}
+
+function repoInformationHTML(repos) {
+    if (repos.length == 0) {
+        return `<div class="clearfix repo-list">No repos!</div>`;
+    }
+
+    let listItemsHTML = repos.map((repo) => {
+        return `<li>
+        <a href="${repo.html_url}" target="_blank">${repo.name}</a>
+        </li>`
+    });
+
+    return `<div class="clearfix repo-list">
+        <p>
+            <strong>Repo List:</strong>
+        </p>
+        <ul>
+            ${listItemsHTML.join("\n")}
+        </ul>
+    </div>`;
+}
+
+$(document).ready(fetchGitHubInformation);
